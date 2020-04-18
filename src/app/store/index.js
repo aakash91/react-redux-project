8ee31b6c74cc41d83975261213de4e632f0e22d7
Aakash <aakash.flyhigh@gmail.com>
2020-04-18 06:24:56 -0400

    commit with frontend setup without a backend

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,8 +1,62 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { defaultState } from '../../server/defaultState'
+import { createLogger } from 'redux-logger'
+import createSagaMiddleware from 'redux-saga';
+
+import * as sagas from './sagas.mock';
+import * as mutations from './mutations';
+
+const sagaMiddleware=createSagaMiddleware();
 
 export const store = createStore(
-    function reducer(state= defaultState, action){
-        return state;
-    }
+    combineReducers({
+        tasks(tasks=defaultState.tasks, action){
+            switch(action.type){
+                case mutations.CREATE_TASK:
+                    console.log("trying to create task");
+                    // console.log(action);
+                    return [...tasks,{
+                        id: action.taskID,
+                        name:"new task",
+                        group: action.groupID,
+                        owner:action.ownerID,
+                        isComplete: false
+                    }]
+                case mutations.SET_TASK_COMPLETE:
+                    console.log("change completion val", action.groupID)    ;
+                    return tasks.map(task => {
+                        return (task.id === action.taskID) ?
+                        {...task, isComplete:action.isComplete} : task;
+                    })
+                case mutations.SET_TASK_NAME:
+                        console.log("change name val", action.groupID)    ;
+                        return tasks.map(task => {
+                            return (task.id === action.taskID) ?
+                            {...task, name:action.name} : task;
+                        })
+                case mutations.SET_TASK_GROUP:
+                    console.log("change group val",action.groupID)    ;
+                    return tasks.map(task => {
+                        return (task.id === action.taskID) ?
+                        {...task, group:action.groupID} : task;
+                    })
+            }
+            return tasks;
+        },
+        comments(comments=defaultState.comments){
+            return comments;
+        },
+        users(users=defaultState.users){
+            return users;
+        },
+        groups(groups=defaultState.groups){
+            return groups;
+        }
+    }),
+    applyMiddleware(createLogger(), sagaMiddleware)
 )
+
+
+for (let saga in sagas){
+    sagaMiddleware.run(sagas[saga])
+}
